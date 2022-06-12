b8616e4a78bc8abd547beb05b90112d55dca6007
Marquis <marquispierre27@gmail.com>
2022-06-11 22:50:59 -0400

    second Commit

--- app.js
+++ app.js
@@ -1,38 +1,75 @@
 const express = require('express')
 const bodyParser= require("body-parser")
 const request = require("request")
-
+const https = require("https")
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
-
+//Gets static Files
 app.use("/public" , express.static(__dirname +"/public"))
-
+// Implements body parser so the HTML can be interacted
 app.use(bodyParser.urlencoded({extended:true}))
 
 
+// gets and return an HTML page/file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/signup.html")
 })
 
+// Post to the server using mailchimp API
+app.post("/" , (req, res)=> {
+  const firstName = req.body.fname
+  const lastName = req.body.lname
+  const email = req.body.email
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data)
+  const  url = "https://us13.api.mailchimp.com/3.0/lists/03a237e833"
+  const options = {
+      method: "POST",
+      auth: "marquis27:API-key"
+  }
+
+     const request = https.request(url,options,function(response){
+      
+         if (response.statusCode == 200) {
+          res.sendFile(__dirname + "/success.html")
+         } else{
+          res.sendFile(__dirname + "/failure.html")
+         }
 
 
-app.post("/" , (req, res)=>{
-  var firstName = req.body.fname
-  var lastName = req.body.lname
-  var email = req.body.email
+       response.on("data" , function (data) { 
+        console.log(JSON.parse(data));
+        })
+     })
+  request.write(jsonData)
+  request.end()
 
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email)
 
 })
-
-
-
-app.listen(port, () =>{
+  
+      
+      app.post("/failure" , (req,res)=>{
+          res.redirect("/")
+       })
+  
+       
+    
+  
+app.listen(port || 3000, () =>{
     console.log(`Server is up and running on ${port}!`)
 } )
 
 
-// 38003f9510ecd7a400fe753857ac3cfe-us13
